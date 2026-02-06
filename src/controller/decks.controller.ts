cea98f53a9472ab84d260a8cc5ce19a03dd4a5fe
Kiizer861 <colin.noe@orange.fr>
2026-02-06 12:31:41 +0100

    feat: Authentification Endpoints et Middleware

---
+++ src/controller/decks.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { authService } from "../service/decks.service";
+
+export const authController = {
+  async signUp(req: Request, res: Response) {
+    try {
+      const { email, username, password } = req.body;
+
+      if (!email || !username || !password) {
+        return res.status(400).json({ message: "Missing fields" });
+      }
+
+      const result = await authService.signUp(email, username, password);
+      return res.status(201).json(result);
+    } catch (error: any) {
+      return res
+        .status(error.status || 500)
+        .json({ message: error.message || "Server error" });
+    }
+  },
+
+  async signIn(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({ message: "Missing fields" });
+      }
+
+      const result = await authService.signIn(email, password);
+      return res.status(200).json(result);
+    } catch (error: any) {
+      return res
+        .status(error.status || 500)
+        .json({ message: error.message || "Server error" });
+    }
+  },
+  getProfile: (req: Request, res: Response) => {
+    res.json({ user: req.user }); 
+  },
+};
